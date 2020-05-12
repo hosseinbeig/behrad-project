bf6aa21eaa8e04ed7d8493b59940b4750205ea1a
Hossein Beigzadeh <hossein@aleph-labs.com>
2020-05-12 12:04:15 +0800

    Navbar Sub updated

--- Home.js
+++ Home.js
@@ -1,3 +1,112 @@
-function OnLinkHover() {
-  document.getElementById("computer").innerHTML = "testing";
+function ComputerClass() {
+  let addComputerClass = document.getElementById("computerList");
+  let addPlayClass = document.getElementById("playList");
+  let addXboxClass = document.getElementById("xBoxList");
+  let addXbox2Class = document.getElementById("xBox2List");
+  let addPlay2Class = document.getElementById("play-2List");
+  let addMobileListClass = document.getElementById("mobileList");
+  let addNewsList = document.getElementById("newsList");
+  addNewsList.classList.remove("showList");
+  addMobileListClass.classList.remove("showList");
+  addPlay2Class.classList.remove("showList");
+  addXbox2Class.classList.remove("showList");
+  addComputerClass.classList.add("showList");
+  addPlayClass.classList.remove("showList");
+  addXboxClass.classList.remove("showList");
+}
+function PlayListClass() {
+  let addPlayClass = document.getElementById("playList");
+  let addComputerClass = document.getElementById("computerList");
+  let addXboxClass = document.getElementById("xBoxList");
+  let addXbox2Class = document.getElementById("xBox2List");
+  let addPlay2Class = document.getElementById("play-2List");
+  let addMobileListClass = document.getElementById("mobileList");
+  let addNewsList = document.getElementById("newsList");
+  addNewsList.classList.remove("showList");
+  addMobileListClass.classList.remove("showList");
+  addPlay2Class.classList.remove("showList");
+  addXbox2Class.classList.remove("showList");
+  addPlayClass.classList.add("showList");
+  addComputerClass.classList.remove("showList");
+  addXboxClass.classList.remove("showList");
+}
+function XboxClass() {
+  let addXboxClass = document.getElementById("xBoxList");
+  let addPlayClass = document.getElementById("playList");
+  let addComputerClass = document.getElementById("computerList");
+  let addXbox2Class = document.getElementById("xBox2List");
+  let addPlay2Class = document.getElementById("play-2List");
+  let addMobileListClass = document.getElementById("mobileList");
+  let addNewsList = document.getElementById("newsList");
+  addNewsList.classList.remove("showList");
+  addMobileListClass.classList.remove("showList");
+  addPlay2Class.classList.remove("showList");
+  addXbox2Class.classList.remove("showList");
+  addXboxClass.classList.add("showList");
+  addPlayClass.classList.remove("showList");
+  addComputerClass.classList.remove("showList");
+}
+function XboxTwoClass() {
+  let addXbox2Class = document.getElementById("xBox2List");
+  let addXboxClass = document.getElementById("xBoxList");
+  let addPlayClass = document.getElementById("playList");
+  let addComputerClass = document.getElementById("computerList");
+  let addPlay2Class = document.getElementById("play-2List");
+  let addMobileListClass = document.getElementById("mobileList");
+  let addNewsList = document.getElementById("newsList");
+  addNewsList.classList.remove("showList");
+  addMobileListClass.classList.remove("showList");
+  addPlay2Class.classList.remove("showList");
+  addXbox2Class.classList.add("showList");
+  addXboxClass.classList.remove("showList");
+  addPlayClass.classList.remove("showList");
+  addComputerClass.classList.remove("showList");
+}
+function PlayListTwoClass() {
+  let addPlay2Class = document.getElementById("play-2List");
+  let addXbox2Class = document.getElementById("xBox2List");
+  let addXboxClass = document.getElementById("xBoxList");
+  let addPlayClass = document.getElementById("playList");
+  let addComputerClass = document.getElementById("computerList");
+  let addMobileListClass = document.getElementById("mobileList");
+  let addNewsList = document.getElementById("newsList");
+  addNewsList.classList.remove("showList");
+  addMobileListClass.classList.remove("showList");
+  addPlay2Class.classList.add("showList");
+  addXbox2Class.classList.remove("showList");
+  addXboxClass.classList.remove("showList");
+  addPlayClass.classList.remove("showList");
+  addComputerClass.classList.remove("showList");
+}
+function MobileListClass() {
+  let addMobileListClass = document.getElementById("mobileList");
+  let addPlay2Class = document.getElementById("play-2List");
+  let addXbox2Class = document.getElementById("xBox2List");
+  let addXboxClass = document.getElementById("xBoxList");
+  let addPlayClass = document.getElementById("playList");
+  let addComputerClass = document.getElementById("computerList");
+  let addNewsList = document.getElementById("newsList");
+  addNewsList.classList.remove("showList");
+  addMobileListClass.classList.add("showList");
+  addPlay2Class.classList.remove("showList");
+  addXbox2Class.classList.remove("showList");
+  addXboxClass.classList.remove("showList");
+  addPlayClass.classList.remove("showList");
+  addComputerClass.classList.remove("showList");
+}
+function NewsListClass() {
+  let addNewsList = document.getElementById("newsList");
+  let addMobileListClass = document.getElementById("mobileList");
+  let addPlay2Class = document.getElementById("play-2List");
+  let addXbox2Class = document.getElementById("xBox2List");
+  let addXboxClass = document.getElementById("xBoxList");
+  let addPlayClass = document.getElementById("playList");
+  let addComputerClass = document.getElementById("computerList");
+  addNewsList.classList.add("showList");
+  addMobileListClass.classList.remove("showList");
+  addPlay2Class.classList.remove("showList");
+  addXbox2Class.classList.remove("showList");
+  addXboxClass.classList.remove("showList");
+  addPlayClass.classList.remove("showList");
+  addComputerClass.classList.remove("showList");
 }
